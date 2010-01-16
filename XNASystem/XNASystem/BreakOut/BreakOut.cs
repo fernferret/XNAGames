3abeca3b9c70a23e8a4bb2a4d175cf2611522dd2
Eric Stokes <fernferret@gmail.com>
2010-01-16 17:30:01 -0500

    Re-Created iGame branch as GameInterface Branch

---
+++ XNASystem/XNASystem/BreakOut/BreakOut.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XNASystem.BreakOut
+{
+	class BreakOut : IGame
+	{
+		public void AdvanceLevel()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ResetGame()
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<PowerUp> GetPowerUps()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Draw()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void FinishGame()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void StartGame()
+		{
+			throw new NotImplementedException();
+		}
+
+		public int GetLevelScore()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ResetScore()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ XNASystem/XNASystem/IGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XNASystem
+{
+	interface IGame
+	{
+		void AdvanceLevel();
+		void ResetGame();
+		List<PowerUp> GetPowerUps();
+		void Draw();
+		void FinishGame();
+		void StartGame();
+		int GetLevelScore();
+		void ResetScore();
+	}
+}
