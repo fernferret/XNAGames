f97d4856182f0e5a3f6cc12da2f8eb073c53c498
Eric Stokes <fernferret@gmail.com>
2010-01-23 14:20:21 -0500

    Added more changes to the displays

--- XNASystem/XNASystem/Displays/GameDisplay.cs
+++ XNASystem/XNASystem/Displays/GameDisplay.cs
@@ -1,6 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
 namespace XNASystem.Displays
 {
-	public class GameDisplay
+	public class GameDisplay: IScreen
 	{
+		public void Update(KeyboardState state)
+		{
+			//throw new NotImplementedException();
+		}
+
+		public void Draw(SpriteBatch spriteBatch, List<SpriteFont> fonts, List<Texture2D> textures)
+		{
+			spriteBatch.Begin();
+			spriteBatch.DrawString(fonts[0], "Game", new Vector2(100, 200), Color.Black);
+			spriteBatch.End();
+		}
 	}
 }
