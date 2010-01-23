be0231f1b1d135186fef53c124df60259a189db1
Aaron Blankenbaker <blankaaro@gmail.com>
2010-01-23 15:08:54 -0500

    updated block and level

--- XNASystem/XNASystem/BreakOut/BreakOutBlock.cs
+++ XNASystem/XNASystem/BreakOut/BreakOutBlock.cs
@@ -1,11 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+
+#region
+
+enum Blocktype
+{
+	Standard,  //Dead after one hit from the ball
+	Invincible,  //never dies
+	Strong2,  //Dead after two hits
+	Strong3,  //Dead after three hits
+	Dead  //No longer affects the ball in any way and not displayed on the screen
+}
+
+#endregion
 
 namespace XNASystem.BreakOut
 {
+	#region
+
 	class BreakOutBlock
 	{
+		private readonly int _xPosition;
+		private readonly int _yPosition;
+		private readonly Blocktype _type;
+
+		public BreakOutBlock(int xPosition, int yPosition, Blocktype type)
+		{
+			_xPosition = xPosition;
+			_yPosition = yPosition;
+			_type = type;
+		}
+
+		public Blocktype Type
+		{
+			get { return _type; }
+		}
+
+		public int YPosition
+		{
+			get { return _yPosition; }
+		}
+
+		public int XPosition
+		{
+			get { return _xPosition; }
+		}
 	}
+
+	#endregion
+
 }
--- XNASystem/XNASystem/BreakOut/BreakOutLevel.cs
+++ XNASystem/XNASystem/BreakOut/BreakOutLevel.cs
@@ -7,7 +7,37 @@ namespace XNASystem.BreakOut
 {
 	class BreakOutLevel
 	{
-		private List<BreakOutBlock> _blocks;
+		private readonly string _name;
+		private readonly List<BreakOutBlock> _blocks;
 
+		public BreakOutLevel(String name)
+		{
+			_name = name;
+		}
+
+		public string Name
+		{
+			get { return _name; }
+		}
+
+		public void AddBlock(BreakOutBlock block)
+		{
+			if (block != null)
+			{
+				//removes any blocks occupying the same location as the block being added
+				foreach(b in _blocks)
+				{
+					if(block.XPosition == b.XPosition)
+					{
+						if(block.YPosition == b.YPosition)
+						{
+							_blocks.Remove(b);
+						}
+					}
+				}
+
+				_blocks.Add(block);
+			}
+		}
 	}
 }
